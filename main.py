325b7c2e4767158adcd75bffe756bf8137570cf7
Gaurav Agarwal <gagarwal89@gmail.com>
2025-10-06 20:48:09 -0700

    switch to oai agents sdk

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from infobip import send_message
+import asyncio
 from openai import OpenAI
+from agents.extensions.models.litellm_model import LitellmModel
+
+from agents import Agent, Runner, function_tool, set_tracing_disabled
 from dotenv import load_dotenv
 import os
+
+
+set_tracing_disabled(True)
 
 # Load environment variables from .env.local first, then .env as fallback
 load_dotenv('.env.local')
@@ -26,7 +32,21 @@ def get_response(message: str):
     history.append({"role": "assistant", "content": response.choices[0].message.content})
     return response.choices[0].message.content
 
+@function_tool
+def get_weather(city: str):
+    print(f"[debug] getting weather for {city}")
+    return f"The weather in {city} is sunny."
+
+async def main():
+    agent = Agent(
+        name="Assistant",
+        instructions="You only respond in haikus.",
+        model=LitellmModel(model=f"ollama/{os.getenv('OLLAMA_MODEL')}", api_key=os.getenv("OLLAMA_API_KEY")),
+        tools=[],
+    )
+ 
+    result = await Runner.run(agent, "What's the weather in Tokyo?")
+    print(result.final_output)
+
 if __name__ == "__main__":
-    agent_response =get_response("What is the capital of France?")
-    print(agent_response)
-    send_message("16462203228", "This is a preregistered test message from Infobip. Enjoy your free trial!")
+    asyncio.run(main())
